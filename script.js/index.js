eba3c66b2023220f287c2a7a6a2844ce28016aab
RamyOtman98-CS <ramy.formal98@gmail.com>
2022-10-05 10:05:21 +0300

    UPD JS

--- script.js/index.js
+++ script.js/index.js
@@ -1,7 +1,9 @@
 function flipLeft() {
   document.getElementById("3d-cube").style.transform = "rotateY(0.5turn)";
+
+  document.getElementById("Front").style.visibility = "visible";
   document.getElementById("Left").style.visibility = "hidden";
-  document.getElementById("Front").style.visibility = "visiable";
+
   setTimeout(() => {
     reverse("Front");
     reverse("Left");
@@ -10,18 +12,19 @@ function flipLeft() {
 
 function flipRight() {
   document.getElementById("3d-cube").style.transform = "rotateY(0.0turn)";
-  document.getElementById("Left").style.visibility = "visiable";
+  document.getElementById("Left").style.visibility = "visible";
   document.getElementById("Front").style.visibility = "hidden";
   setTimeout(() => {
     reverse("Front");
     reverse("Left");
   }, 999);
 }
+
 function reverse(id) {
   const elm = document.getElementById(id);
-  if (elm === "Front") {
+  if (elm.style.visibility === "visible") {
     elm.style.visibility = "hidden";
   } else {
-    elm.style.visibility = "visiable";
+    elm.style.visibility = "visible";
   }
 }
